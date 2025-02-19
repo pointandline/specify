fec395d8740a4518bd97a4cc12faeba9a1ab25f0
oaao <oliver+github@abolish.golf>
2025-02-18 23:14:36 -0700

    rev [ui] animations: slower self-disappearing

--- specify/components/SphereAnimation.js
+++ specify/components/SphereAnimation.js
@@ -124,7 +124,7 @@ const SphereAnimation = ({ onButtonClick }) => {
         targets: sphereButtonContainerRef.current,
         opacity: 0,
         filter: 'blur(200px)',
-        duration: 2000,
+        duration: 3000,
         easing: 'easeInOutSine',
       });
     }
