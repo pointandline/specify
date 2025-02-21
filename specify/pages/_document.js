6a93de430403135c08e693252bafa70651e28cd9
oaao <oliver+github@abolish.golf>
2025-02-21 16:27:40 -0700

    fix [deploy] favicon: explicitly ref favicon so it shows on github pages

--- specify/pages/_document.js
+++ specify/pages/_document.js
@@ -6,6 +6,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* pathing fix for github pages favicon */}
+        <link rel="shortcut icon" type="image/x-icon" href="specify/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
