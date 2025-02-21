10b61a5e43d0b4eeb7acbc553b445106e2f4da8f
oaao <oliver+github@abolish.golf>
2025-02-21 16:44:30 -0700

    fix [pages] Head: camel-case crossOrigin attr/prop

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
         {/* pathing fix for github pages favicon */}
         <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,100..900;1,100..900&display=swap"
