c3338b5f65a405d426551248ca9abd7b64ccd240
oaao <oliver+github@abolish.golf>
2025-02-21 15:29:03 -0700

    add [deploy]: configs for single-command github pages deploy

--- specify/next.config.mjs
+++ specify/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // this disables default image optimization
+  },
+  assetPrefix: isProd ? '/specify' : '',
+  basePath: isProd ? '/specify' : '',
+  output: 'export'  // this enables static exports
 };
 
 export default nextConfig;
