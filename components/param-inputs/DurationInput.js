9da3d807cbc4cf8c18658781310fa076dd206eeb
oaao <oliver+github@abolish.golf>
2025-02-21 18:01:29 -0700

    rev [ui] DurationInput: implement duration input, UI only

--- components/param-inputs/DurationInput.js
+++ components/param-inputs/DurationInput.js
@@ -1,23 +1,16 @@
 import { useState } from 'react';
-import Grid from '@mui/material/Box';
-import InputAdornment from '@mui/material/InputAdornment';
-import MuiInput from '@mui/material/Input';
+import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 
 import styles from '@/styles/DurationInput.module.css'
 
 
-const maxHours = 100
-const maxDuration = maxHours * 60 * 60 * 1000 // expressed in milliseconds
+const maxMinutes = 100 * 60 // the longest Spotify song is ~48hrs but we'll just do 100
+const maxDuration = maxMinutes * 60 * 60 * 1000 // expressed in milliseconds
 
 export function DurationInput({ param, suggestedRange = [] }) {
   const { name, rightIcon, leftIcon } = param;
-  const durationPropsMap = [
-      ["hh", 100],
-      ["mm", 60],
-      ["ss", 60],
-  ]
-  const [inputValue, setInputValue] = useState(0)
 
   const sharedProps = {
     'className': styles.smallNumberInput,
@@ -25,24 +18,24 @@ export function DurationInput({ param, suggestedRange = [] }) {
     'variant': 'outlined',
     'size': 'small',
     'type': 'number',
-    'sx': {'color': 'secondary.dark', 'fontWeight': 700, }
+    'width': "10%",
+    'textAlign': "right",
+    'sx': {'color': 'secondary.dark', 'fontWeight': 700 }
   }
 
   return (
     <>
-      <Grid container spacing={2}>
-          <TextField {...sharedProps} label="hh" InputProps={{ inputProps: {min: 0, max: 100} }} />
-          <TextField {...sharedProps} label="mm" InputProps={{ inputProps: {min: 0, max: 60} }} />    
-          <TextField {...sharedProps} label="ss" InputProps={{ inputProps: {min: 0, max: 60} }} />
-      </Grid>
+      {/* TODO: should have near-primary text opacity for the input boxes themselves */}
+      <Stack>
+        <TextField {...sharedProps} label="mm" inputProps={{ min: 0, max: maxMinutes, style: { textAlign: "right"} }} />    
+        <TextField {...sharedProps} label="ss" inputProps={{ min: 0, max: 60, style: { textAlign: "right"} }} />
+      </Stack>
+      <Typography variant="body1" color="text.primary" sx={{ px: 2 }}>to</Typography>
+      <Stack>
+        <TextField {...sharedProps} label="mm" inputProps={{ min: 0, max: maxMinutes, style: { textAlign: "right"} }} />    
+        <TextField {...sharedProps} label="ss" inputProps={{ min: 0, max: 60, style: { textAlign: "right"} }} />
+      </Stack>
     </>
+
   );
 }
-
-
-{/*
-
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="filled-basic" label="Filled" variant="filled" />
-        <TextField id="standard-basic" label="Standard" variant="standard" />
-*/}
